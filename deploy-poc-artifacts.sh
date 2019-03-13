ea59c30b87b1720a6b7d412387e02a93a8e75c70
Alexey Kazakov <alkazako@redhat.com>
2019-03-12 17:05:41 -0700

    Remove compositions CM

--- deploy-poc-artifacts.sh
+++ deploy-poc-artifacts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #kubectl.sh create -f artifacts/example/ns.yaml
-#kubectl.sh create configmap -n kubepoc kind-compositions-config-map --from-file=kind_compositions.yaml
 #kubectl.sh create -f artifacts/example/sa.yaml -n kubepoc
 #kubectl.sh create -f artifacts/example/auth-delegator.yaml -n kube-system
 #kubectl.sh create -f artifacts/example/auth-reader.yaml -n kube-system
@@ -11,7 +10,6 @@
 #kubectl.sh create -f artifacts/example/apiservice.yaml
 #FOR MINIKUBE UNCOMMENT THESE: todo: code to automate this
 kubectl create -f artifacts/example/ns.yaml
-kubectl create configmap -n kubepoc kind-compositions-config-map --from-file=kind_compositions.yaml
 kubectl create -f artifacts/example/sa.yaml -n kubepoc
 kubectl create -f artifacts/example/auth-delegator.yaml -n kube-system
 kubectl create -f artifacts/example/auth-reader.yaml -n kube-system
